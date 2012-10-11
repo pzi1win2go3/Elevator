2043e71add35f3cb4940543ae2d129f0e3b380e5
郑淇木 <zheng.qm@163.com>
2012-10-11 11:21:59 +0800

    上传代码
    泊松分布随机数产生函数

---
+++ material/Poisson.cpp
@@ -0,0 +1,27 @@
+# include "Poisson.h"
+
+double U_Rand(double a, double b)		//    均匀分布   
+{
+	const int MAX_VAL = 32767;
+	double x = rand();   
+	return    a + (b - a) * (x + 1) / (MAX_VAL + 1);   
+}   
+    
+double P_Rand(double Lamda)				//    泊松分布   
+{
+	double x = -1, u;  
+	double log1, log2;
+
+	log1 = 0;
+	log2 = -Lamda;
+
+   do
+   {
+		u = U_Rand(0, 1);   
+	   log1 += log(u);
+		 x++;   
+   }
+   while(log1 >= log2);
+
+   return    x;   
+}  

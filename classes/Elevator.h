78787bd6de37501e635b1c8f526f8a2e38057208
郑淇木 <zheng.qm@163.com>
2012-10-11 11:43:27 +0800

    更新头文件
    完成初步声明

---
+++ classes/Elevator.h
@@ -0,0 +1,29 @@
+# include <iostream>
+# include "Mission.h"
+using namespace std;
+
+# ifndef ELEVATOR_H
+# define ELEVATOR_H
+
+class Elevator
+{
+public:
+	Elevator ( );				// undesigned
+	void takeMission(Mission *);
+	void move();
+	void action();
+	void complete();
+	void open();
+	void close();
+private:
+	Mission* mission;
+	int missionNum;			// 改进算法时使用
+	int position;
+	int status;
+	int passenger;
+	bool isEmpty;
+	int id;
+
+};
+
+# endif

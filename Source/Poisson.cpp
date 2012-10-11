b33b73718c93d485bffab69aea435e83caa61888
pzi1win2go3 <paranoidmonkeyzhao@gmail.com>
2012-10-11 13:24:29 +0800

    fix

--- Source/Poisson.cpp
+++ Source/Poisson.cpp
@@ -4,7 +4,7 @@ double U_Rand(double a, double b)		//    均匀分布
 {
 	const int MAX_VAL = 32767;
 	double x = rand();   
-	return    a + (b - a) * (x + 1) / (MAX_VAL + 1);   
+	return a + (b - a) * (x + 1) / (MAX_VAL + 1);   
 }   
     
 double P_Rand(double Lamda)				//    泊松分布   
@@ -15,13 +15,13 @@ double P_Rand(double Lamda)				//    泊松分布
 	log1 = 0;
 	log2 = -Lamda;
 
-   do
-   {
+  do
+  {
 		u = U_Rand(0, 1);   
 	   log1 += log(u);
 		 x++;   
-   }
-   while(log1 >= log2);
+  }
+  while(log1 >= log2);
 
-   return    x;   
+  return x;   
 }  
